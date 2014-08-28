3da4a8e57e75a758ed9253cc2681db1c8263e7d7
Jindrich Vimr <jvimr@softeu.com>
2014-08-28 22:13:14 +0200

    dalsi upravy - potlaceni zbytecnych vypisu

--- update-usernames.sh
+++ update-usernames.sh
@@ -2,19 +2,21 @@
 
 . ./.local-settings
 
+PATH=/usr/sbin:/sbin:$PATH
+
 #get username list
 
-wget -N "$SRC_URL/usernames.txt"
+wget -qN "$SRC_URL/usernames.txt"
 
 
 for USERNAME in `cat usernames.txt`
 do
-  echo "user $USERNAME"
+#  echo "user $USERNAME"
   if [ ! -d /home/$USERNAME ]
   then
 
-    echo "user does not exists"
-    wget -N "$SRC_URL/$USERNAME"
+    echo "user $USERNAME does not exists"
+    wget -qN "$SRC_URL/$USERNAME"
     
   #pokud se nam podari najit cele jmeno, pouzijeme ho
     if [ -f $USERNAME ]
@@ -28,13 +30,13 @@ do
 #$    nakonec vytvorime uzivatele
 
     adduser --disabled-password --shell /bin/bash --quiet --gecos "$FULLNAME,,,"  $USERNAME
-    echo "user created $?"
+    echo "user $USERNAME created $?"
   fi
 
 done
 
 #nakonec zmenime hesla
 
-wget -N "$SRC_URL/passwords.txt"
+wget -qN "$SRC_URL/passwords.txt"
 
 chpasswd < passwords.txt
